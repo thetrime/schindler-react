bdad3be91f0be97a018a5421d575760c35ed670f
Matt Lilley <matt.lilley@securitease.com>
2016-02-15 05:16:57 +1300

    Update todo

--- schindler.jsx
+++ schindler.jsx
@@ -20,6 +20,7 @@ ReactDOM.render(<div className="root vertical_layout">
       * If you tick an item off the list and you are at an unknown store, we need to prompt the user to identify the store. Allow them to say 'I forgot', and in that case, do not prompt them for an aisle as well
       * Also if all locations are deleted we would end up here
    * Undo
+   * Add foreign key references to schema to add resilience
    * Details screen for items
    * Secure password management (or at least not totally insecure management!)
    * Gzip large items
