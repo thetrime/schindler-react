9315ff2103d5403d5533bf5e0c581d39a6f87587
Matt Lilley <thetrime@gmail.com>
2018-10-11 16:21:28 +0100

    Fix compile error. oops

--- api.pl
+++ api.pl
@@ -5,6 +5,6 @@
 add_item(Request):-
         memberchk(search(Search), Request),
         memberchk(key=Key, Search),
-        http_read_data(Request, Item, [])
+        http_read_data(Request, Item, []),
         handle_message(Key, want_item, _{name:Item}),
         format('Content-Type: text/plain~n~nOK, added', []).
