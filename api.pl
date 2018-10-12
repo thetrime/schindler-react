3bb5646fc1a2c1b7e3b310f16bc166ac21ae23e3
Matt Lilley <thetrime@gmail.com>
2018-10-12 16:24:28 +0100

    Tweak

--- api.pl
+++ api.pl
@@ -7,5 +7,9 @@ add_item(Request):-
         memberchk(search(Search), Request),
         memberchk(key=Key, Search),
         http_read_data(Request, Item, []),
-        handle_message(Key, want_item, _{name:Item}),
-        format('Content-Type: text/plain~n~nOK, added', []).
+        ( list_item(Key, Item)->
+            format('Content-Type: text/plain~n~n~w is already on the list', [Item])
+        ; otherwise->
+            handle_message(Key, want_item, _{name:Item}),
+            format('Content-Type: text/plain~n~nOK, added', [])
+        ).
